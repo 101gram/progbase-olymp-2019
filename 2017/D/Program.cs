2a446328c8a85c8d33e384b80efa2fdaead4c10f
Arkadiy <arkadiy@ex.ua>
2019-04-04 01:43:15 +0300

    task D

---
+++ 2017/D/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace D {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(GetNumberSubarrays(3, new int[] {1,2,3,4,5}));
+        }
+
+        static int GetNumberSubarrays(int height, int[] heights) {
+            int result = 0;
+            for (int begin = 0; begin < heights.Length; begin++) {
+                for (int end = begin; end < heights.Length; end++) {
+                    if (IsValidHeigth(ref heights, begin, end, height)) {
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
+
+        static bool IsValidHeigth(ref int[] array, int begin, int end, int val) {
+            return array[begin + ((end - begin + 2) / 2 - 1)] >= val;
+        }
+    }
+}

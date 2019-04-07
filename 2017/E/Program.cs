b1805e7ef87791b0e89a8677ae1d4dfa942f5d46
Arkadiy <arkadiy@ex.ua>
2019-04-07 16:32:28 +0300

    task E

---
+++ 2017/E/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace E {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(countAllWays(4));
+        }
+
+        static long countAllWays(int length) {
+            return fibonacci(length).last;
+        }
+
+        static (long penultimate, long last) fibonacci(
+            int count, long penultimate = 1, long last = 2, int i = 3
+        ) {
+            if (i == count) {
+                return (penultimate, last);
+            }
+            return fibonacci(count, last, penultimate + last, ++i);
+        }
+    }
+}
